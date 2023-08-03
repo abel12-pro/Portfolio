33680664900df588c82ac3faa120026efe2c9a1d
Abel <abelu2013@gmail.com>
2023-08-03 15:37:03 +0300

    Added data to local storage.

--- script.js
+++ script.js
@@ -243,4 +243,27 @@ document.addEventListener('DOMContentLoaded', () => {
   // preserve data in the browser
   const name = form.querySelector('#name');
   const message = form.querySelector('#message');
+
+  const storedData = localStorage.getItem('userData');
+  const convertStoredData = storedData ? JSON.parse(storedData) : {};
+  if (convertStoredData) {
+    name.value = convertStoredData.name || '';
+    email.value = convertStoredData.email || '';
+    message.value = convertStoredData.message || '';
+  }
+  function updateFormData() {
+    const nameValue = name.value;
+    const emailValue = email.value;
+    const messageValue = message.value;
+    const formData = {
+      name: nameValue,
+      email: emailValue,
+      message: messageValue,
+    };
+    const jsonFormData = JSON.stringify(formData);
+    localStorage.setItem('userData', jsonFormData);
+  }
+  name.addEventListener('input', updateFormData);
+  email.addEventListener('input', updateFormData);
+  message.addEventListener('input', updateFormData);
 });
